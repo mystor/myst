cd80e21a060b83081ad5e80bc8f1ed0d3b629859
Michael Layzell <michael@thelayzells.com>
2014-09-16 15:19:25 -0400

    Add a bunch of functions to the runtime

--- runtime.js
+++ runtime.js
@@ -1,40 +1,78 @@
 var immutable = require('immutable');
 
 function add(a, b) {
-  if (typeof a !== 'number' || typeof b !== 'number') throw new Error('Cannot add non-numbers');
+  if (+a !== a || +b !== b) throw new Error('Cannot add non-numbers');
   return a + b;
 }
 
 function sub(a, b) {
-  if (typeof a !== 'number' || typeof b !== 'number') throw new Error('Cannot sub non-numbers');
+  if (+a !== a || +b !== b) throw new Error('Cannot sub non-numbers');
   return a - b;
 }
 
 function mul(a, b) {
-  if (typeof a !== 'number' || typeof b !== 'number') throw new Error('Cannot mul non-numbers');
+  if (+a !== a || +b !== b) throw new Error('Cannot mul non-numbers');
   return a * b;
 }
 
 function div(a, b) {
-  if (typeof a !== 'number' || typeof b !== 'number') throw new Error('Cannot div non-numbers');
+  if (+a !== a || +b !== b) throw new Error('Cannot div non-numbers');
   return a / b;
 }
 
+function negate(a) {
+  if (+a !== a) throw new Error('Cannot negate non-numbers');
+  return -a;
+}
+
 function modulo(a, b) {
-  if (typeof a !== 'number' || typeof b !== 'number') throw new Error('Cannot modulo non-numbers');
+  if (+a !== a || +b !== b) throw new Error('Cannot modulo non-numbers');
   return a % b;
 }
 
 function binAnd(a, b) {
-  if (typeof a !== 'number' || typeof b !== 'number') throw new Error('Cannot binAnd non-numbers');
+  if (+a !== a || +b !== b) throw new Error('Cannot binAnd non-numbers');
   return a & b;
 }
 
 function binOr(a, b) {
-  if (typeof a !== 'number' || typeof b !== 'number') throw new Error('Cannot binOr non-numbers');
+  if (+a !== a || +b !== b) throw new Error('Cannot binOr non-numbers');
   return a | b;
 }
 
+function binNot(a) {
+  if (+a !== a) throw new Error('Cannot binNot non-numbers');
+  return ~a;
+}
+
+function eq(a, b) {
+  return a === b; // TODO: Support deep equals in immutablejs objects
+}
+
+function neq(a, b) {
+  return a !== b; // TODO: Support deep equals in immutablejs objects
+}
+
+function lte(a, b) {
+  return a <= b;
+}
+
+function gte(a, b) {
+  return a >= b;
+}
+
+function lt(a, b) {
+  return a < b;
+}
+
+function gt(a, b) {
+  return a > b;
+}
+
+// Determine if a value is truthy (in Myst terms)
+function truth(x) {
+  return x != null && x !== false;
+}
 
 
 function get(object, property) {
@@ -54,5 +92,21 @@ function get(object, property) {
 
 module.exports = {
   G: get,
-  get: get
+  get: get,
+  truth: truth,
+  add: add,
+  sub: sub,
+  mul: mul,
+  div: div,
+  negate: negate,
+  modulo: modulo,
+  binAnd: binAnd,
+  binOr: binOr,
+  binNot: binNot,
+  eq: eq,
+  neq: neq,
+  lte: lte,
+  gte: gte,
+  lt: lt,
+  gt: gt
 };
