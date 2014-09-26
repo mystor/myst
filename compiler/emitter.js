8f2fb8a662545b342b3171962c35579d2cfc83c6
Michael Layzell <michael@thelayzells.com>
2014-09-26 10:22:55 -0400

    Major modifications to the emitter

--- compiler/emitter.js
+++ compiler/emitter.js
@@ -1,5 +1,35 @@
 var ast = require('./ast');
+var immut = require('immutable');
 var Syntax = ast.Syntax;
+
+function __rt_import(options) {
+  return {
+    type: 'VariableDeclaration',
+    declarations: [
+      {
+        type: 'VariableDeclarator',
+        id: {
+          type: 'Identifier',
+          name: '__rt'
+        },
+        init: {
+          type: 'CallExpression',
+          callee: {
+            type: 'Identifier',
+            name: 'require'
+          },
+          arguments: [
+            {
+              type: 'Literal',
+              value: options.runtime || 'myst/runtime'
+            }
+          ]
+        }
+      }
+    ],
+    kind: 'var'
+  };
+}
 
 function __rt_dot(name) {
   return {
@@ -16,62 +46,58 @@ function __rt_dot(name) {
   };
 }
 
+function expr(handler) {
+  return function(ast, ctx) {
+    var handled = handler(ast, ctx.merge({ statement: false, return: false }));
+    if (ctx.get('statement')) {
+      if (ctx.get('return')) {
+        return {
+          type: 'ReturnStatement',
+          argument: handled
+        };
+      } else {
+        return {
+          type: 'ExpressionStatement',
+          expression: handled
+        };
+      }
+    } else {
+      return handled;
+    }
+  };
+}
+
 function isStatement(x) {
   return Syntax.isBasicDeclaration(x) || Syntax.isImport(x) || Syntax.isDeclaration(x);
 }
 
 function makeEmitter(options) {
   var emits = {
-    Program: function(program) {
-      return { // TODO Imports
+    Program: function(program, ctx) {
+      var cCtx = ctx.merge({
+        statement: true,
+        toplevel: true,
+        return: false
+      });
+
+      return {
         type: 'Program',
-        body: [
-          {
-            type: 'VariableDeclaration',
-            declarations: [
-              {
-                type: 'VariableDeclarator', // -- Runtime
-                id: {
-                  type: 'Identifier',
-                  name: '__rt'
-                },
-                init: {
-                  type: 'CallExpression',
-                  callee: {
-                    type: 'Identifier',
-                    name: 'require'
-                  },
-                  arguments: [
-                    {
-                      type: 'Literal',
-                      value: options.runtime || 'myst/runtime'
-                    }
-                  ]
-                }
-              }
-            ],
-            kind: 'var'
-          }
-        ].concat(program.body.map(function(stmt) {
-          if (isStatement(stmt)) {
-            return emit(stmt);
-          } else {
-            return {
-              type: 'ExpressionStatement',
-              expression: emit(stmt)
-            };
-          }
+        body: [__rt_import(options)].concat(program.body.map(function(stmt) {
+          return emit(stmt, cCtx);
         }))
       };
     },
 
-    Import: function(req) {
+    Import: function(req, ctx) {
+      if (! ctx.get('toplevel') || ! ctx.get('statement'))
+        throw new Error('Can only import values in the toplevel scope');
+
       return {
         type: 'VariableDeclaration',
         declarations: [
           {
             type: 'VariableDeclarator',
-            id: emit(req.as),
+            id: emit(req.as, ctx.merge({ statement: false, toplevel: false })),
             init: {
               type: 'CallExpression',
               callee: {
@@ -79,7 +105,7 @@ function makeEmitter(options) {
                 name: 'require'
               },
               arguments: [
-                emit(req.resource)
+                emit(req.resource, ctx.merge({ statement: false, toplevel: false }))
               ]
             }
           }
@@ -88,51 +114,58 @@ function makeEmitter(options) {
       };
     },
 
-    Identifier: function(identifier) {
+    Identifier: expr(function(identifier, ctx) {
       return {
         type: 'Identifier',
         name: identifier.name
       };
-    },
+    }),
 
-    Literal: function(literal) {
+    Literal: expr(function(literal, ctx) {
       return {
         type: 'Literal',
         value: literal.value
       };
-    },
+    }),
 
-    BasicDeclaration: function(decl) {
+    BasicDeclaration: function(decl, ctx) {
+      var target = emit(decl.target, ctx.merge({ statement: false }));
+      var init = emit(decl.value, ctx.merge({ statement: false }));
+      if (ctx.get('toplevel')) {
+        init = {
+          type: 'AssignmentExpression',
+          operator: '=',
+          left: {
+            type: 'MemberExpression',
+            object: { type: 'Identifier', name: 'exports' },
+            property: target,
+            computed: false
+          },
+          right: init
+        };
+      }
+
       return {
         type: 'VariableDeclaration',
         declarations: [
           {
             type: 'VariableDeclarator',
-            id: emit(decl.target),
-            init: {
-              type: 'AssignmentExpression',
-              operator: '=',
-              left: {
-                type: 'MemberExpression',
-                object: { type: 'Identifier', name: 'exports' },
-                property: emit(decl.target),
-                computed: false
-              },
-              right: emit(decl.value) // Desugarer should ensure all Decls have 1 statement
-            }
+            id: target,
+            init: init
           }
         ],
         kind: 'var'
       };
     },
 
-    Invocation: function(invocation) {
+    Invocation: expr(function(invocation, ctx) {
+      console.log(invocation.arguments, emit(invocation.arguments, ctx));
       return {
         type: 'CallExpression',
         callee: {
           type: 'MemberExpression',
           computed: false,
-          object: emit(invocation.callee),
+          object: emit(invocation.callee, ctx),
           property: {
             type: 'Identifier',
             name: 'call'
@@ -143,56 +176,55 @@ function makeEmitter(options) {
             type: 'Literal',
             value: null
           }
-        ].concat(emit(invocation.arguments))
+        ].concat(emit(invocation.arguments, ctx))
       };
-    },
+    }),
 
-    Operation: function(operation) {
+    Operation: expr(function(operation, ctx) {
       switch (operation.name) {
       case 'or':
         return {
           type: 'LogicalExpression',
           operator: '||',
-          left: emit(operation.fst),
-          right: emit(operation.snd)
+          left: emit(operation.fst, ctx),
+          right: emit(operation.snd, ctx)
         };
 
       case 'and':
         return {
           type: 'LogicalExpression',
           operator: '&&',
-          left: emit(operation.fst),
-          right: emit(operation.snd)
+          left: emit(operation.fst, ctx),
+          right: emit(operation.snd, ctx)
         };
 
       default:
         throw new Error('Can only emit && and || operators');
       }
-    },
+    }),
 
-    Lambda: function(lambda) {
-      var body = lambda.body.map(function(stmt, idx) {
-        if (isStatement(stmt)) {
-          return emit(stmt);
-        } else {
-          if (idx === lambda.body.length - 1) {
-            return {
-              type: 'ReturnStatement',
-              argument: emit(stmt)
-            };
-          } else {
-            return {
-              type: 'ExpressionStatement',
-              expression: emit(stmt)
-            };
-          }
-        }
+    Lambda: expr(function(lambda, ctx) {
+      var last = lambda.body[lambda.body.length - 1];
+      var start = lambda.body.slice(0, lambda.body.length - 1);
+
+      var body = start.map(function(stmt) {
+        return emit(stmt, ctx.merge({
+          toplevel: false,
+          statement: true,
+          return: false
+        }));
       });
+
+      body.push(emit(last, ctx.merge({
+        toplevel: false,
+        statement: true,
+        return: true
+      })));
 
       return {
         type: 'FunctionExpression',
         id: null,
-        params: emit(lambda.parameters),
+        params: emit(lambda.parameters, ctx), // TODO: Handle better?
         defaults: [],
         body: {
           type: 'BlockStatement',
@@ -202,116 +234,73 @@ function makeEmitter(options) {
         generator: false,
         expression: false
       };
-    },
+    }),
 
-    If: function(ifExpr) {
-      if (ifExpr.consequent.length === 1 &&
-          ifExpr.alternate.length === 1) {
-        return {
-          type: 'ConditionalExpression',
-          test: emit(ifExpr.cond),
-          consequent: emit(ifExpr.consequent[0]),
-          alternate: emit(ifExpr.alternate[0])
+    If: function(ifExpr, ctx) {
+      var cond = emit(ifExpr.cond, ctx.merge({
+        statement: false,
+        return: false,
+        toplevel: false
+      }));
+
+      if (ctx.get('statement')) {
+        return {  // TODO: Don't emit else branch unless necessary
+          type: 'IfStatement',
+          test: cond,
+          consequent: {
+            type: 'BlockStatement',
+            body: emit(ifExpr.consequent, ctx.merge({ toplevel: false }))
+          },
+          alternate: {
+            type: 'BlockStatement',
+            body: emit(ifExpr.alternate, ctx.merge({ toplevel: false }))
+          }
         };
       } else {
-        var thn = ifExpr.consequent.map(function(maybeExpr) {
-          if (isStatement(maybeExpr)) {
-            return emit(maybeExpr);
-          } else {
-            return {
-              type: 'ExpressionStatement',
-              expression: emit(maybeExpr)
-            };
-          }
-        });
-
-        if (thn.length &&
-            thn[thn.length - 1].type === 'ExpressionStatement') {
-          thn[thn.length - 1] = {
-            type: 'ReturnStatement',
-            argument: thn[thn.length - 1].expression
+        if (ifExpr.consequent.length === 1 && ifExpr.alternate.length === 1) {
+          // Can be represented as a ternary expression
+          return {
+            type: 'ConditionalExpression',
+            test: cond,
+            consequent: emit(ifExpr.consequent[0], ctx),
+            alternate: emit(ifExpr.alternate[0], ctx)
           };
+        } else {
+          return emit(Syntax.Invocation( // Wrap in an immediately-invoked function
+            Syntax.Lambda([], [ifExpr]),
+            []
+          ), ctx);
         }
-
-        var els = ifExpr.alternate.map(function(maybeExpr) {
-          if (isStatement(maybeExpr)) {
-            return emit(maybeExpr);
-          } else {
-            return {
-              type: 'ExpressionStatement',
-              expression: emit(maybeExpr)
-            };
-          }
-        });
-
-        if (els.length &&
-            els[els.length - 1].type === 'ExpressionStatement') {
-          els[els.length - 1] = {
-            type: 'ReturnStatement',
-            argument: els[els.length - 1].expression
-          };
-        }
-
-        return {
-          type: 'CallExpression',
-          callee: {
-            type: 'FunctionExpression',
-            id: null,
-            params: [],
-            defaults: [],
-            body: {
-              type: 'BlockStatement',
-              body: [
-                {
-                  type: 'IfStatement',
-                  test: emit(ifExpr.cond),
-                  consequent: {
-                    type: 'BlockStatement',
-                    body: thn
-                  },
-                  alternate: {
-                    type: 'BlockStatement',
-                    body: els
-                  }
-                }
-              ]
-            },
-            rest: null,
-            generator: false,
-            expression: false
-          },
-          arguments: []
-        };
       }
     },
 
-    Member: function(member) {
+    Member: expr(function(member, ctx) {
       return {
         type: 'MemberExpression',
-        object: emit(member.object),
+        object: emit(member.object, ctx),
         property: {
           type: 'Literal',
           value: member.property
         },
         computed: true
       };
-    },
+    }),
 
-    Method: function(method) {
+    Method: expr(function(method, ctx) {
       return {
         type: 'CallExpression',
         callee: __rt_dot('G'),
         arguments: [
-          emit(method.object),
+          emit(method.object, ctx),
           {
             type: 'Literal',
             value: method.property
           }
         ]
       };
-    },
+    }),
 
-    Object: function(object) {
+    Object: expr(function(object, ctx) {
       return {
         type: 'CallExpression',
         callee: __rt_dot('M'),
@@ -321,40 +310,42 @@ function makeEmitter(options) {
             properties: object.properties.map(function(property) {
               return {
                 type: 'Property',
-                key: emit(property.key),
-                value: emit(property.value),
+                key: emit(property.key, ctx),
+                value: emit(property.value, ctx),
                 kind: 'init'
               };
             })
           }
         ]
       };
-    },
+    }),
 
-    Array: function(array) {
+    Array: expr(function(array, ctx) {
       return {
         type: 'CallExpression',
         callee: __rt_dot('V'),
-        arguments: emit(array.items)
+        arguments: emit(array.items, ctx)
       };
-    }
+    })
   };
 
-  function emit(ast) {
+  function emit(ast, ctx) {
     if (Array.isArray(ast))
-      return ast.map(emit);
+      return ast.map(function(ast) { return emit(ast, ctx); });
 
     if (! emits.hasOwnProperty(ast.type))
       throw new Error('The type: ' + ast.type + ' is not supported');
 
-    return emits[ast.type](ast);
+    return emits[ast.type](ast, ctx);
   }
 
   return emit;
 };
 
 function emit(ast, options) {
-  return makeEmitter(options)(ast);
+  var em = makeEmitter(options)(ast, immut.Map());
+  console.log(JSON.stringify(em, null, 2));
+  return em;
 }
 
 module.exports = {
